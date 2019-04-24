2bf063fc09ea2d2333aebaf82f45b0c33fd161d9
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-24 14:02:30 +0600

    add action

---
+++ forum-frontend/src/containers/PostsBuilder/PostsBuilder.js
@@ -0,0 +1,49 @@
+import React, {Component, Fragment} from 'react';
+import {connect} from "react-redux";
+import {fetchPosts} from "../../store/actions/postActions";
+
+import Posts from "../../components/Posts/Posts";
+
+
+class PostsBuilder extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    getPost = id => {
+        this.props.history.push({
+            pathname: '/posts/' + id
+        })
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <h1>Posts</h1>
+                {this.props.posts.map(post => (
+                    <Posts
+                        key={post._id}
+                        image={post.image}
+                        datetime={post.datetime}
+                        user={post.user}
+                        title={post.title}
+                        onClick={() => this.getPost(post._id)}/>
+                ))}
+            </Fragment>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        posts: state.post.posts
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchPosts: () => dispatch(fetchPosts())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsBuilder);
