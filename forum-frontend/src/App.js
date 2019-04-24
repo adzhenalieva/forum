bff59b5154bf7f32013e2b49b2887eb9b0742aad
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-24 11:32:52 +0600

    add Login and Register component

--- forum-frontend/src/App.js
+++ forum-frontend/src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import {Switch, Route, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {logOutUser} from "./store/actions/userActions";
+import Container from "reactstrap/es/Container";
+
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <header>
+                    <Toolbar
+                        user={this.props.user}
+                        logout={this.props.logOutUser}
+                    />
+                </header>
+                <Container className="mt-5">
+                    <Switch>
+                        {/*<Route path="/" exact component={ForumBuilder}/>*/}
+                        <Route path="/register" exact component={Register}/>
+                        <Route path="/login" exact component={Login}/>
+                    </Switch>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    user: state.user.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    logOutUser: () => dispatch(logOutUser())
+});
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- forum-frontend/src/index.js
+++ forum-frontend/src/index.js
@@ -1,10 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {createBrowserHistory} from 'history';
+import {connectRouter, routerMiddleware, ConnectedRouter} from 'connected-react-router';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import userReducer from "./store/reducers/userReducer";
+
+
+const saveToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (e) {
+        console.log('Could not save state');
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        return undefined;
+    }
+};
+
+const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+    router: connectRouter(history),
+    user: userReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [
+    thunkMiddleware,
+    routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(rootReducer, persistedState, enhancers);
+
+store.subscribe(() => {
+    saveToLocalStorage({
+        user: {
+            user: store.getState().user.user
+        }
+
+    })
+});
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
